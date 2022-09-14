413cd3e6553e342109248861880c2f6cfe5350d2
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-14 11:00:09 -0500

    setup the react store and reducers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'root/index'
+
+   namespace :api do
+    resources :greetings, only: [:index]
+  end
+
+  root 'root#index'
+
+   get '*greet', to: "root#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
